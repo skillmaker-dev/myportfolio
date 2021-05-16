672dbfdd6788947e5f154f0e7920d3eccb249fc4
Anas Chahid <anas.chahid777@gmail.com>
2021-05-16 00:50:40 +0000

    updated skills block

--- js/main.js
+++ js/main.js
@@ -5,4 +5,19 @@ collapsibles.forEach((item) =>
   })
 );
 
+const card = document.querySelectorAll(".skill-card");
+const movingcard = document.querySelectorAll(".skill__description");
 
+
+card.forEach((item) => item.addEventListener('mouseover' || 'click', () => {
+
+  movingcard.forEach((item) => item.style.backgroundColor = colors());
+  console.log(colors());
+
+
+}));
+
+function colors() {
+  let colorArray = ["#0E6BA8","#5B1865","#6A8EAE","#474935","#D00000"];    
+  return colorArray[Math.floor(Math.random() * colorArray.length)]
+}

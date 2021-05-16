616e7282f00a276d35fc96e684b103cff03109ff
Anas Chahid <anas.chahid777@gmail.com>
2021-05-16 16:14:40 +0100

    added hiding navbar

--- js/main.js
+++ js/main.js
@@ -21,3 +21,14 @@ function colors() {
   let colorArray = ["#0E6BA8","#5B1865","#6A8EAE","#474935","#D00000"];    
   return colorArray[Math.floor(Math.random() * colorArray.length)]
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-65px";
+  }
+  prevScrollpos = currentScrollPos;
+}

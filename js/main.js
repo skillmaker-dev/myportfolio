7bfdd28567b3f22bebb928479ba6ff5151093ae3
skillmaker <anas.chahid777@gmail.com>
2022-06-09 23:10:43 +0100

    changed background

--- js/main.js
+++ js/main.js
@@ -8,22 +8,20 @@ collapsibles.forEach((item) =>
 const card = document.querySelectorAll(".skill-card");
 const movingcard = document.querySelectorAll(".skill__description");
 
-
-card.forEach((item) => item.addEventListener('mouseover' || 'click', () => {
-
-  movingcard.forEach((item2) => item2.style.backgroundColor = colors());
-  console.log(colors());
-
-
-}));
+card.forEach((item) =>
+  item.addEventListener("mouseover" || "click", () => {
+    movingcard.forEach((item2) => (item2.style.backgroundColor = colors()));
+    console.log(colors());
+  })
+);
 
 function colors() {
-  let colorArray = ["#0E6BA8","#5B1865","#6A8EAE","#474935","#D00000"];    
-  return colorArray[Math.floor(Math.random() * colorArray.length)]
+  let colorArray = ["#0E6BA8", "#5B1865", "#6A8EAE", "#474935", "#D00000"];
+  return colorArray[Math.floor(Math.random() * colorArray.length)];
 }
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
@@ -31,4 +29,4 @@ window.onscroll = function() {
     document.getElementById("navbar").style.top = "-65px";
   }
   prevScrollpos = currentScrollPos;
-}
+};
